ad765b5cc5432615c94defc41b3722b44bfcfefa
mhhasanmehedi <mmmehedi53@gmail.com>
2021-11-18 23:43:23 +0600

    something

--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { useHistory, useParams } from 'react-router';
-import PostData from '../../PostData.json';
-import { Helmet } from 'react-helmet';
-import './Posts.scss';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router";
+import PostData from "../../PostData.json";
+import { Helmet } from "react-helmet";
+import "./Posts.scss";
+import { Link } from "react-router-dom";
 
 const Posts = () => {
   const { postId } = useParams();
@@ -15,9 +15,9 @@ const Posts = () => {
         <title>Post || {posts[0].title}</title>
       </Helmet>
       <div>
-        <div className='single-post'>
+        <div className="single-post">
           <h3>{posts[0].title}</h3>
-          <img width='100%' height='400px' src={posts[0].img} alt='' />
+          <img width="100%" height="400px" src={posts[0].img} alt="" />
           <p>{posts[0].title}</p>
           <p>
             Link Link: <a href={posts[0].live}>{posts[0].live}</a>
@@ -25,8 +25,8 @@ const Posts = () => {
           <p>
             Github Link: <a href={posts[0].github}>{posts[0].github}</a>
           </p>
-          <Link to='/home'>
-            <button type='button'>Go home</button>
+          <Link to="/home" className="btn">
+            <button type="button">Go home</button>
           </Link>
         </div>
       </div>
